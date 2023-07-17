5728f3d6dc91336c0896fdef1f7f12f6ee615e81
Raimundo Soares <raimundojssoares@gmail.com>
2023-07-17 20:52:40 -0300

    responsivity

--- app/components/getintouch/contact/FormContacts.tsx
+++ app/components/getintouch/contact/FormContacts.tsx
@@ -1,7 +1,7 @@
 export default function FormContacts() {
   return (
     <form id="contact" className="flex items-center justify-center ">
-      <div className="flex items-center justify-center w-[340px] sm:w-[900px] sm:h-[620px] bg-gray-200  shadow-xl rounded-3xl">
+      <div className="flex items-center justify-center w-[400px] sm:w-[900px] sm:h-[620px] bg-gray-200  shadow-xl rounded-3xl">
         <div>
           <div className="flex items-center justify-center flex-wrap mb-8 font-semibold text-2xl">
             <h1>CONTACT</h1>
@@ -10,24 +10,24 @@ export default function FormContacts() {
             type="text"
             id="name"
             placeholder="Name"
-            className="mb-6 flex items-center justify-center flex-wrap w-[500px] h-12 rounded-lg pl-4 bg-zinc-100 outline-gray-500 text-zinc-800 text-xl font-sans"
+            className="mb-6 flex items-center justify-center flex-wrap sm:w-[500px] h-12 rounded-lg pl-4 bg-zinc-100 outline-gray-500 text-zinc-800 text-xl font-sans"
           />
           <input
             type="email"
             id="email"
             placeholder="Email"
-            className="mb-6 flex items-center justify-center flex-wrap w-[500px] h-12 rounded-lg pl-4 bg-zinc-100 outline-gray-500 text-zinc-800 text-xl font-sans"
+            className="mb-6 flex items-center justify-center flex-wrap sm:w-[500px] h-12 rounded-lg pl-4 bg-zinc-100 outline-gray-500 text-zinc-800 text-xl font-sans"
           />
           <input
             type="text"
             id="subject"
             placeholder="Subject"
-            className="mb-6 flex items-center justify-center flex-wrap w-[500px] h-12 rounded-lg pl-4 bg-zinc-100 outline-gray-500 text-zinc-800 text-xl font-sans"
+            className="mb-6 flex items-center justify-center flex-wrap sm:w-[500px] h-12 rounded-lg pl-4 bg-zinc-100 outline-gray-500 text-zinc-800 text-xl font-sans"
           />
 
           <textarea
             placeholder="Message"
-            className=" pl-4 pt-4 w-[500px] h-[200px] rounded-md resize-none bg-zinc-100  outline-gray-500 text-zinc-800 text-xl font-sans "
+            className=" pl-4 pt-4 sm:w-[500px] h-[200px] rounded-md resize-none bg-zinc-100  outline-gray-500 text-zinc-800 text-xl font-sans "
           />
 
           <div
